148e90c1f41b7c32dac2c42bf8da7a581d09f8e0
Collin R <collin.codes.hard@gmail.com>
2023-04-25 14:22:16 -0500

    Created function to replace letters w/ symbol and added event listener for click

--- js/script.js
+++ js/script.js
@@ -5,6 +5,30 @@ const textInput = document.querySelector(".letter");
 const inProgress = document.querySelector(".word-in-progress");
 const remainingGuesses = document.querySelector(".remaining");
 const span = document.querySelector(".remaining span");
-const guessedLetters = document.querySelector(".message");
+const message = document.querySelector(".message");
 const playAgain = document.querySelector(".play-again");
 
+const word = "magnolia"; 
+//Magnolia= starting word to test out game
+
+
+//Create function to update "words-in-progress" paragraph & replace letters with symbol
+placeholder = function (word) {
+    const placeholderLetters = [];
+    for (const letter of word) {
+        console.log(letter);
+        placeholderLetters.push("●")
+    }
+    inProgress.innerText = placeholderLetters.join("");
+
+};
+placeholder(word);
+
+//Add event listener for the button 
+//This clears guess box after submitting & logs input in console
+button.addEventListener("click", function (e) {
+    e.preventDefault();
+    const guessedLetters = textInput.value;
+    console.log(guessedLetters);
+    textInput.value = "";
+});
