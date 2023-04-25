f3df895302c02f3aabcf4be4e11350d2229ebe47
Collin R <collin.codes.hard@gmail.com>
2023-04-25 13:00:00 -0500

    Created global variables

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-f
+//Create global variables
+const list = document.querySelector(".guessed-letters");
+const button = document.querySelector(".guess");
+const textInput = document.querySelector("input");
+const inProgress = document.querySelector(".word-in-progress");
+const remainingGuesses = document.querySelector(".remaining");
+const span = document.querySelector("span");
+const guessedLetters = document.querySelector(".message");
+const playAgain = document.querySelector(".play-again .hide");
+
