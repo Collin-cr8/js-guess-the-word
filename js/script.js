ca09d4383d2461b1c157eb6af2ddc9452909eeb0
Collin R <collin.codes.hard@gmail.com>
2023-04-25 13:02:33 -0500

    Updated variables

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 //Create global variables
 const list = document.querySelector(".guessed-letters");
 const button = document.querySelector(".guess");
-const textInput = document.querySelector("input");
+const textInput = document.querySelector(".letter");
 const inProgress = document.querySelector(".word-in-progress");
 const remainingGuesses = document.querySelector(".remaining");
-const span = document.querySelector("span");
+const span = document.querySelector(".remaining span");
 const guessedLetters = document.querySelector(".message");
-const playAgain = document.querySelector(".play-again .hide");
+const playAgain = document.querySelector(".play-again");
 
